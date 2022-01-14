18e8e7470f004c45ada0e3333523e3ba8488aa1b
plmqazoknijb <w2033@e-mirim.hs.kr>
2022-01-14 11:58:19 +0900

    기출문제 2차 풀이 완료_2 Commit

--- 2급 파이썬 문제/2차 2급 1_initial_code.py
+++ 2급 파이썬 문제/2차 2급 1_initial_code.py
@@ -3,13 +3,13 @@ max_product_number = 10
 def func_a(gloves):
     counter = [0 for _ in range(max_product_number + 1)]
     for x in gloves:
-        @@@
+        counter[x] += 1
     return counter
 
 def solution(left_gloves, right_gloves):
     left_counter = func_a(left_gloves)
     right_counter = func_a(right_gloves)
-    
+
     total = 0
     for i in range(1, max_product_number + 1):
         total += min(left_counter[i], right_counter[i])
